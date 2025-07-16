af61d45532fd2772beb5a1cbba1a0ab74c7699ce
kino <1491037864@qq.com>
2025-07-16 22:42:51 +0800

    Refactor blog and layout, improve UI consistency
    Replaced <section> with <article> in blog.html, standardized class names, improved comment and interaction bar markup, and added sticky header script. Updated script loading to use 'defer' for better performance. Refined CSS for blog, common, global, index, login, note, and server pages to improve layout, color variables, and responsive design. Enhanced header.js for sticky behavior and active link management.

--- scripts/header.js
+++ scripts/header.js
@@ -1,27 +1,25 @@
-'use strict'
-const active = document.querySelector('.active')
-const navList = document.querySelector('.nav-list')
-const main = document.querySelector('main')
+"use strict";
+const active = document.querySelector(".active");
+const navList = document.querySelector(".nav-list");
+const main = document.querySelector("main");
 
-navList.addEventListener('click', function (event) {
-  if (event.target.tagName === 'a') {
-    const activeLink = navList.querySelector('.active')
-    if (activeLink) {
-      activeLink.classList.remove('active')
+navList.addEventListener("click", function (event) {
+    if (event.target.tagName === "a") {
+        const activeLink = navList.querySelector(".active");
+        if (activeLink) {
+            activeLink.classList.remove("active");
+        }
+        event.target.classList.add("active");
     }
-    event.target.classList.add('active')
-  }
-})
+});
 
-const header = document.querySelector('.header')
-const stickyThreshold = 100 // 设定滚动的阈值，超过 100px 才变为 sticky
+const header = document.querySelector(".header");
+const stickyThreshold = 100;
 
-window.addEventListener('scroll', () => {
-  if (window.scrollY > stickyThreshold) {
-    // 当滚动超过阈值时，添加 sticky 类
-    header.classList.add('sticky')
-  } else {
-    // 当滚动小于阈值时，移除 sticky 类
-    header.classList.remove('sticky')
-  }
-})
+window.addEventListener("scroll", () => {
+    if (window.scrollY > stickyThreshold) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+});
